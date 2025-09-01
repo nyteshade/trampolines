8052073a140957c2da0c94c59b9d2420c58900aa
Brielle Harrison <nyteshade@gmail.com>
2025-08-31 21:54:56 -0700

    Attempt next x86

--- trampoline_x86.c
+++ trampoline_x86.c
@@ -25,46 +25,40 @@ void *trampoline_create(void *target_func, void *context, size_t public_argc) {
 
   unsigned char *c = mem;
 
-  // For x86 cdecl: all args on stack, we need to insert context as first arg
+  // For x86 cdecl: we need to prepend context to the argument list
   // Stack on entry: [ret_addr][arg0][arg1]...
-  // We need to call target with: [ret_addr][context][arg0][arg1]...
+  // We want:        [ret_addr][context][arg0][arg1]...
   
-  if (public_argc == 0) {
-    // No arguments to preserve, just push context
-    *c++ = 0x68;                          // push imm32
-    memcpy(c, &context, 4); c += 4;
-  } else {
-    // We have arguments to preserve
-    // The trick: we'll use a call instruction which will push a new return address
-    // Then inside the target, it will return to our cleanup code
-    
-    // Push context as first argument
-    *c++ = 0x68;                          // push imm32
-    memcpy(c, &context, 4); c += 4;
-    
-    // Call the target function (this pushes return address)
-    *c++ = 0xE8;                          // call rel32
-    int32_t rel = (int32_t)((intptr_t)target_func - ((intptr_t)(c + 4)));
-    memcpy(c, &rel, 4); c += 4;
-    
-    // After target returns, clean up the context we pushed
-    *c++ = 0x83; *c++ = 0xC4; *c++ = 0x04;  // add esp, 4
-    
-    // Return to original caller
-    *c++ = 0xC3;                          // ret
-    
-    // Make sure we don't fall through to the code below
-    if (mprotect(mem, ps, PROT_READ | PROT_EXEC) != 0) {
-      munmap(mem, ps);
-      return NULL;
-    }
-    return mem;
+  // Method: use the standard function prologue and manipulate arguments
+  *c++ = 0x55;                          // push ebp
+  *c++ = 0x89; *c++ = 0xE5;            // mov ebp, esp
+  
+  // Now stack is: [saved_ebp][ret_addr][arg0][arg1]...
+  // ebp+4 = return address
+  // ebp+8 = first arg
+  // ebp+12 = second arg, etc.
+  
+  // Push all original arguments in reverse order (rightmost first)
+  for (int i = public_argc - 1; i >= 0; i--) {
+    *c++ = 0xFF; *c++ = 0x75; *c++ = (unsigned char)(8 + i * 4);  // push [ebp + 8 + i*4]
   }
   
-  // Jump to target (for no-arg case)
-  *c++ = 0xE9;                          // jmp rel32
-  int32_t rel = (int32_t)((intptr_t)target_func - ((intptr_t)c + 4));
+  // Push context as first argument
+  *c++ = 0x68;                          // push imm32
+  memcpy(c, &context, 4); c += 4;
+  
+  // Call target function
+  *c++ = 0xE8;                          // call rel32
+  int32_t rel = (int32_t)((intptr_t)target_func - ((intptr_t)(c + 4)));
   memcpy(c, &rel, 4); c += 4;
+  
+  // Clean up stack (context + original args)
+  unsigned char cleanup = (public_argc + 1) * 4;
+  *c++ = 0x83; *c++ = 0xC4; *c++ = cleanup;  // add esp, cleanup
+  
+  // Standard function epilogue
+  *c++ = 0x5D;                          // pop ebp
+  *c++ = 0xC3;                          // ret
 
   // RX permissions
   if (mprotect(mem, ps, PROT_READ | PROT_EXEC) != 0) {
