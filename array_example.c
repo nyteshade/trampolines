54ab246407cf8934069a07c5b6a46c918320d382
Brielle Harrison <nyteshade@gmail.com>
2025-09-01 02:05:34 -0700

    Reverting i386 code and adding verbosity to array_example

--- array_example.c
+++ array_example.c
@@ -10,10 +10,16 @@ int main(int argc, char **argv) {
   PtrArray* array = PtrArrayMake(2);
   
   array->append("Hello world");
+  printf("Added element (%lu size, %lu capacity)", array->size(), array->capacity());
+  
   array->append("Goodbye cruel world");
+  printf("Added element (%lu size, %lu capacity)", array->size(), array->capacity());
+  
   array->append("Laters");
+  printf("Added element (%lu size, %lu capacity)", array->size(), array->capacity());
   
   printf("Last element is %s\n", array->last());
+  printf("Element at index 1 is %s\n", array->elementAt(1));
   printf("First element is %s\n", array->first());
   
   array->free();
--- trampoline_x86.c
+++ trampoline_x86.c
@@ -18,6 +18,8 @@ static size_t page_size(void) {
 }
 
 void *trampoline_create(void *target_func, void *context, size_t public_argc) {
+  (void)public_argc; // all args are stack-passed on i386 SysV
+
   const size_t ps   = page_size();
   unsigned char *mem = (unsigned char *)mmap(
       NULL, ps, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
@@ -25,39 +27,21 @@ void *trampoline_create(void *target_func, void *context, size_t public_argc) {
 
   unsigned char *c = mem;
 
-  // For x86 cdecl: we need to prepend context to the argument list
-  // Stack on entry: [ret_addr][arg0][arg1]...
-  // We want:        [ret_addr][context][arg0][arg1]...
-  
-  // Method: use the standard function prologue and manipulate arguments
-  *c++ = 0x55;                          // push ebp
-  *c++ = 0x89; *c++ = 0xE5;            // mov ebp, esp
-  
-  // Now stack is: [saved_ebp][ret_addr][arg0][arg1]...
-  // ebp+4 = return address
-  // ebp+8 = first arg
-  // ebp+12 = second arg, etc.
-  
-  // Push all original arguments in reverse order (rightmost first)
-  for (int i = public_argc - 1; i >= 0; i--) {
-    *c++ = 0xFF; *c++ = 0x75; *c++ = (unsigned char)(8 + i * 4);  // push [ebp + 8 + i*4]
-  }
-  
-  // Push context as first argument
-  *c++ = 0x68;                          // push imm32
+  // prologue: save original return in ECX, insert context, then CALL target
+  *c++ = 0x59;                          // pop ecx                 ; save caller_ret
+  *c++ = 0x68;                          // push imm32 (context)
   memcpy(c, &context, 4); c += 4;
-  
-  // Call target function
-  *c++ = 0xE8;                          // call rel32
-  int32_t rel = (int32_t)((intptr_t)target_func - ((intptr_t)(c + 4)));
-  memcpy(c, &rel, 4); c += 4;
-  
-  // Clean up stack (context + original args)
-  unsigned char cleanup = (public_argc + 1) * 4;
-  *c++ = 0x83; *c++ = 0xC4; *c++ = cleanup;  // add esp, cleanup
-  
-  // Standard function epilogue
-  *c++ = 0x5D;                          // pop ebp
+
+  // call rel32 target_func
+  {
+    *c++ = 0xE8;                        // call rel32
+    int32_t rel = (int32_t)((intptr_t)target_func - ((intptr_t)c + 4));
+    memcpy(c, &rel, 4); c += 4;
+  }
+
+  // epilogue: drop injected context, restore caller_ret, return
+  *c++ = 0x83; *c++ = 0xC4; *c++ = 0x04; // add esp, 4             ; pop context
+  *c++ = 0x51;                          // push ecx                ; push caller_ret
   *c++ = 0xC3;                          // ret
 
   // RX permissions
